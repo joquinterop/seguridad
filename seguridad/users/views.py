75595b602d0f67a4d4186a63a82a0efbbbdbe5da
joquinterop <jo.quintero@duocuc.cl>
2024-11-30 18:27:16 -0300

    actualizacio_de_login

--- seguridad/users/views.py
+++ seguridad/users/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('home')
         else:
             return HttpResponse("Credenciales inválidas")
-    return render(request, 'users/login.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
 
 @login_required
 def home(request):
